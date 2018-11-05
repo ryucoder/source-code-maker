28637e8bf1dd79c88eec6f783e740f8f330a8742
RyuCoder <ganji.chetan@gmail.com>
2018-11-05 14:04:02 +0530

    Class names retrieved.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import sys 
 import django 
 import inspect
+import  importlib   
 
+from django.views import generic
 from pprint import pprint
 
 from SourceCodeMaker import SourceCodeMaker
@@ -10,6 +12,8 @@ from File001 import Hero, Base, Zero
 
 files = []
 directories = []
+modules = []
+classes = []
 
 
 def list_files_and_folders(folder):
@@ -25,54 +29,69 @@ def list_files_and_folders(folder):
 
 
 def extract_module_strings_from_files():
-    for one_file in files:
-        print(one_file)
+    path = 'C:\\Program Files\\Python36\\lib\\site-packages\\django\\views\\generic'
+    remove = 'C:\Program Files\Python36\lib\site-packages\\'
+    
+    list_files_and_folders(path)
+
+    for name in files:
+        module_name = name.replace(remove, '').replace(".py", "")
+        if not module_name.endswith("__init__"):
+            modules.append(module_name.replace("\\", "."))
+
+
+def extract_classes_from_modules():
+    
+    for item in modules:
+        # for some reasons dates file is not being imported
+        if item != "django.views.generic.dates":
+
+            filename = importlib.import_module(item)
+
+            for item in inspect.getmembers(filename):
+                if not item[0].startswith("__"):
+                    # print(item)
+                    if inspect.isclass(item[1]):
+                        # print(item[0])
+                        classes.append(item[1])
+                        # print(inspect.getsource(item[1]))
 
 def main():
     print()
     # source = SourceCodeMaker(Hero)
     # print(source.final_source_code)
+    # django_path = os.path.dirname(django.__file__)
+
+    # list_files_and_folders(django_path)
+
+    # print(len(files))
+
+    # count = 0
+    # extensions = []
+    # for f in files: 
+    #     count += 1
+    #     parts = f.split(".")
+    #     # print(len(parts))
+    #     # print(len(extensions))
+    #     # print(count)
+    #     if count == 728:
+    #         continue
+    #     # if parts[-1].startswith("."):
+    #     if parts[-1] == "py":
+    #         extensions.append(parts)
+    # print(len(extensions))
+
+    extract_module_strings_from_files()
+    extract_classes_from_modules()
     
-    django_path = os.path.dirname(django.__file__)
-
-    list_files_and_folders(django_path)
-
-    print(len(files))
-
-    count = 0
-    extensions = []
-    for f in files: 
-        count += 1
-        parts = f.split(".")
-        # print(len(parts))
-        # print(len(extensions))
-        # print(count)
-        if count == 728:
-            continue
-        # if parts[-1].startswith("."):
-        if parts[-1] == "py":
-            extensions.append(parts)
-    print(len(extensions))
-    
-    # pprint(set(extensions))
-    # pprint(extensions)
-    # print(set(extensions))
-        # if count == 10:
-        #     break
-
-    # import importlib
-
-    # x = importlib.import_module("django.shortcuts")
-
-    # for item in inspect.getmembers(x):
-    #     if not item[0].startswith("__"):
-    #         # print(item)
-    #         if inspect.isclass(item[1]):
-    #             print(item[0])
-
+    pprint(classes)
+    pprint(len(classes))
 
 if __name__ == "__main__":
     main()
+
+
+
 
     # get source of super methods from mro if super is called in any methods
 
